f04c2ff6953ff8d038a69a5eccf69770c7b468c8
Rudi Siarudin <rudi.siarudin@gmail.com>
2022-07-29 17:07:54 +0700

    First commit

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LocationController;
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\AssetController;
+use App\Http\Controllers\ProcurementController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+
+
+Auth::routes(['verify' => true]);
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('profile', ProfileController::class);
+    Route::resource('user', UserController::class);
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::resource('location', LocationController::class);
+    Route::resource('categories', CategoriesController::class);
+    Route::resource('asset', AssetController::class);
+    Route::resource('procurement', ProcurementController::class);
+    Route::delete('location/{id}', [LocationController::class, 'delete'])->name('location.delete');
+    Route::get('/comingsoon', function () {
+        return view('comingsoon');
+    });
+    Route::get('/', function () {
+        return view('home');
+    });
+});
